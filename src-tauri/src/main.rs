6c3ae52db2a6f30173e5c8031664ae16e257ab12
haiyang <phycgp@gmail.com>
2021-08-31 23:38:09 +0800

    feat: create db and insert data

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,10 +3,10 @@
   windows_subsystem = "windows"
 )]
 
-use std::fs::File;
+use std::fs::{File, create_dir_all};
 use std::io::{self, BufRead};
 use std::path::Path;
-use rusqlite::{Connection, Result};
+use rusqlite::{params, Connection, Result};
 
 fn create_db(db_path: &str) -> Result<Connection> {
   let conn = Connection::open(db_path)?;
@@ -14,6 +14,13 @@ fn create_db(db_path: &str) -> Result<Connection> {
   conn.execute("CREATE TABLE log (entry TEXT)", [])?;
 
   Ok(conn)
+}
+
+fn insert_line(conn: &Connection, entry: String) -> Result<usize> {
+    conn.execute(
+       "INSERT INTO log (entry) VALUES (?1)",
+       params![entry],
+    )
 }
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
@@ -38,8 +45,11 @@ fn parse_file(file_path: String) -> Result<(), String> {
     .ok_or("Invalid file path.")?;
   let mut db_path = tauri::api::path::data_dir().unwrap();
   db_path.push(app_name);
+  let db_dir_path = db_path.as_path();
+  create_dir_all(db_dir_path);
   db_path.push(file_stem);
   db_path.set_extension("db");
+  let db = create_db(db_path.to_str().unwrap()).expect("Failed to open DB");
 
   println!("got file_path: {}", file_path);
   println!("use db_path: {:?}", db_path);
@@ -47,7 +57,7 @@ fn parse_file(file_path: String) -> Result<(), String> {
   if let Ok(lines) = read_lines(&file_path) {
     for line in lines {
       if let Ok(entry) = line {
-        // println!("{}", entry);
+          insert_line(&db, entry);
       }
     }
   }
